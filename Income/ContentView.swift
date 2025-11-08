0eb4bd75906adf01ef07f55241a4ba475aac35b4
Terje Moe <terjemoe54@hotmail.com>
2025-11-08 11:11:38 +0100

    Initial Commit

---
+++ Income/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Income
+//
+//  Created by Terje Moe on 08/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Income/IncomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeApp.swift
+//  Income
+//
+//  Created by Terje Moe on 08/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct IncomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
