a54f2df6c327f6afe252547ea8234872e3499834
Terje Moe <terjemoe54@hotmail.com>
2025-11-09 10:09:47 +0100

    Endringer i settings View

--- Income/Views/SettingsView.swift
+++ Income/Views/SettingsView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct SettingsView: View {
     
-    @State private var name: String = ""
-    @State private var tax: String = ""
-    @State private var account: String = ""
-    
+    @AppStorage("Your Name") var name: String = ""
+    @AppStorage("Your Bank Account") var account: String = ""
+    @AppStorage("TaxPercent") var tax: String = ""
     var body: some View {
        ZStack {
            Color.yellow.opacity(0.2)
@@ -20,13 +19,22 @@ struct SettingsView: View {
            VStack{
                
                Text("SETTINGS")
+                   .font(.largeTitle)
+                   .fontWeight(.bold)
+                   .background(.green.opacity(0.2))
                    .padding(.top, 30)
-               
-               TextField("Name: ", text: $name)
-               TextField("Bank Account:", text: $account)
-               TextField("Tax % :", text: $tax) // Denne må gjøres om til int
-               Spacer()
+                   .padding(.bottom, 30)
+          
+               Text("Navn:")
+                   TextField("Name: ", text: $name)
+                   Text("BankKonto:")
+                   TextField("Bank Account:", text: $account)
+                   Text("SkatteProsent:")
+                   TextField("Tax % :", text: $tax) // Denne må gjøres om til int
+                Spacer()
            }
+           .multilineTextAlignment(.center)
+           .font(.system(size: 16, weight:.bold))
            .textFieldStyle(RoundedBorderTextFieldStyle())
            .cornerRadius(15)
            .padding(.horizontal, 25)
