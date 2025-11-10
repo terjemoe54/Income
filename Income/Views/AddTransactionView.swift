a4618f079f7bccd6eb7b5006f5fed5eea86095a2
Terje Moe <terjemoe54@hotmail.com>
2025-11-10 10:41:18 +0100

    Integrert Floating Button

---
+++ Income/Views/AddTransactionView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTransactionView.swift
+//  Income
+//
+//  Created by Terje Moe on 10/11/2025.
+//
+
+import SwiftUI
+
+struct AddTransactionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTransactionView()
+}
--- Income/Views/HomeView.swift
+++ Income/Views/HomeView.swift
@@ -13,10 +13,29 @@ struct HomeView: View {
         Transaction(title: "Lønn", type: .income, state: .resieved, cat: .ordenary, amount: 32000.54, regDate: Date(), expDate: .now),
         Transaction(title: "Skatt", type: .expense, state: .taken, cat: .ordenary, amount: 9425.78, regDate: .now, expDate: .now),
         Transaction(title: "Husleie", type: .expense, state: .taken, cat: .taken, amount: 11300.00, regDate: .now, expDate: Date()),
-        Transaction(title: "Telefon", type: .expense, state: .pending, cat: .ordenary, amount: 984.45, regDate: .now, expDate: .now),
-        Transaction(title: "Strøm", type: .expense, state: .paid, cat: .ordenary, amount: 387.29, regDate: Date(), expDate: Date()),
-        Transaction(title: "Kemner", type: .expense, state: .taken, cat: .taken, amount: 1370.00, regDate: .now, expDate: .now)
+//        Transaction(title: "Telefon", type: .expense, state: .pending, cat: .ordenary, amount: 984.45, regDate: .now, expDate: .now),
+//        Transaction(title: "Strøm", type: .expense, state: .paid, cat: .ordenary, amount: 387.29, regDate: Date(), expDate: Date()),
+//        Transaction(title: "Kemner", type: .expense, state: .taken, cat: .taken, amount: 1370.00, regDate: .now, expDate: .now)
     ]
+    
+    fileprivate func FloatingButton() -> some View {
+        VStack {
+            Spacer()
+            NavigationLink {
+                AddTransactionView()
+            } label: {
+                Text("+")
+                    .font(.largeTitle)
+                    .frame(width: 70, height: 70)
+                    .foregroundStyle(Color.white)
+                
+            }
+            .background(Color.primaryLightGreen)
+            .clipShape(Circle())
+            .padding(.bottom, 7)
+            
+        }
+    }
     
     fileprivate func BalanceView() -> some View {
         ZStack {
@@ -24,7 +43,7 @@ struct HomeView: View {
                 .fill(Color.primaryLightGreen)
             VStack(alignment: .leading, spacing: 8) {
                 HStack {
-                    VStack {
+                    VStack(alignment: .leading) {
                         Text("BALANCE")
                             .font(.caption)
                             .foregroundStyle(Color.white)
@@ -32,7 +51,7 @@ struct HomeView: View {
                             .font(.system(size: 42, weight: .light))
                             .foregroundStyle(Color.white)
                         }
-                        Spacer()
+                  Spacer()
                 }
                 .padding(.top)
                 
@@ -54,6 +73,7 @@ struct HomeView: View {
                             .foregroundStyle(Color.white)
                     }
                 }
+                
                 Spacer()
             }
             .padding(.horizontal)
@@ -64,16 +84,20 @@ struct HomeView: View {
     }
     
     var body: some View {
-        VStack {
-             BalanceView()
-            List {
-                ForEach(transactions) { transaction in
-                    TransactionView(transaction: transaction)
+        NavigationStack {
+            ZStack {
+                VStack {
+                    BalanceView()
+                    List {
+                        ForEach(transactions) { transaction in
+                            TransactionView(transaction: transaction)
+                        }
+                    }
+                    .scrollContentBackground(.hidden)
                 }
+                FloatingButton()
             }
-            .scrollContentBackground(.hidden)
         }
-        .padding()
     }
 }
 
