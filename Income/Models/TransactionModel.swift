8bedafff05b2b07592e9a31a028ef12a42c9d0a6
Terje Moe <terjemoe54@hotmail.com>
2025-11-19 13:07:57 +0100

    Lakt over til IiCloude Fra Tuen

--- Income/Models/TransactionModel.swift
+++ Income/Models/TransactionModel.swift
@@ -12,13 +12,13 @@ import SwiftData
     @Model
     class TransactionModel {
         
-        var id: UUID
-        var title: String
-        var type: TransactionType
-        var state: TransactionState
-        var amount: Double
-        var regDate: Date
-        var expDate: Date
+        var id: UUID = UUID()
+        var title: String = ""
+        var type: TransactionType = TransactionType.expense
+        var state: TransactionState = TransactionState.pending
+        var amount: Double = 0
+        var regDate: Date = Date()
+        var expDate: Date = Date()
         
         init(id: UUID, title: String, type: TransactionType, state: TransactionState, amount: Double, regDate: Date, expDate: Date) {
             self.id = id
@@ -30,18 +30,20 @@ import SwiftData
             self.expDate = expDate
             
         }
+        
+        @Transient
         var displayRegDate: String {
             let dateFormatter = DateFormatter()
             dateFormatter.dateStyle = .medium
             return dateFormatter.string(from: regDate)
         }
-        
+        @Transient
         var displayExpDate: String {
             let dateFormatter = DateFormatter()
             dateFormatter.dateStyle = .medium
             return dateFormatter.string(from: expDate)
         }
-        
+        @Transient
         var displayAmount: String {
             let numberFormatter = NumberFormatter()
             numberFormatter.numberStyle = .currency
@@ -50,32 +52,3 @@ import SwiftData
         }
     }
                
-//  struct Transaction: Identifiable , Hashable {
-//
-//    let id = UUID()
-//    let title: String
-//    let type: TransactionType
-//    let state: TransactionState
-//    let amount: Double
-//    let regDate: Date
-//    let expDate: Date
-//    
-//    var displayRegDate: String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateStyle = .medium
-//        return dateFormatter.string(from: regDate)
-//    }
-//    
-//    var displayExpDate: String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateStyle = .medium
-//        return dateFormatter.string(from: expDate)
-//    }
-//    
-//    var displayAmount: String {
-//        let numberFormatter = NumberFormatter()
-//        numberFormatter.numberStyle = .currency
-//        numberFormatter.maximumFractionDigits = 2
-//        return numberFormatter.string(from: amount as NSNumber) ?? "$0.00"
-//    }
-//}
